9ac6e13ebfb16baca44ef18aacb0f6a693bd2f35
sashank152 <chandansashank@gmail.com>
2023-08-21 01:20:13 +0800

    Created ecommerce_front

---
+++ ecommerce_front/pages/index.js
@@ -0,0 +1,15 @@
+import Featured from "@/Components/Featured";
+import Header from "@/Components/Header";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header />
+      <Featured />
+    </div>
+  )
+}
+
+export function getServerSideProps(){
+  
+}
